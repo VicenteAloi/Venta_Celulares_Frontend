034b1b71a5000df1d70f04a2b04199eddc48239c
Vicente Aloi <aloivicente@gmail.com>
2024-09-06 21:16:47 -0300

    Cambio de endpoint

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: 'https://ventacelularesbackend-production.up.railway.app/api/products/'
+  endpoint: 'https://ventacelularesbackend-production.up.railway.app/'
 }
 
